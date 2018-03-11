a00a7bc669ddc789df586f4435dba48e4fcaed3e
neimad971 <damientalbot26@gmail.com>
2018-03-11 01:52:46 +0100

    when user (whether client or admin) sign in, he's redirected to the right url + the right link is showing up for either create a new transaction or create a new bank account respectively for admin and client.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,10 @@ class ApplicationController < ActionController::Base
 
   # redirect to right the url according to user role
   def after_sign_in_path_for(resource_or_scope)
-    if current_user
+    if current_user.role.role_tid == Role::CLIENT_ROLE_TID
       client_bank_accounts_url
+    elsif current_user.role.role_tid == Role::ADMIN_ROLE_TID
+      admin_transactions_url
     end
   end
 
